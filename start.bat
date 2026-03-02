ab8a7dade135bb8260fff2becef7fb3155057731
Amar-Yomakesh <amarshimoga90@gmail.com>
2026-03-02 16:56:26 +0530

    initial commit

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Clifden Beauty ^& Laser Clinic website...
+echo.
+echo Open your browser at: http://localhost:3000
+echo Press Ctrl+C to stop.
+echo.
+npx serve . --listen 3000
